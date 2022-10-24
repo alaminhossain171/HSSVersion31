ba725338565e711607886547e32ff15d9046cd5f
alaminhossain171 <48113312+alaminhossain171@users.noreply.github.com>
2022-10-24 14:48:29 +0600

    push notification added

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import Icon from 'react-native-vector-icons/FontAwesome';
+
 const App = () => {
   return (
     <View>
-      <Text><Icon name="rocket" size={30} color="#900" /></Text>
+      <Text>App</Text>
     </View>
   )
 }
